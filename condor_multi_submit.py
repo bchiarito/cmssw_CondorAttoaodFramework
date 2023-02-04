5af69bfa21b19bb5aef435b0f33e15468a6814ba
Brandon Chiarito <bchiarito@gmail.com>
2023-02-03 19:09:30 -0500

    tweaks

--- condor_multi_submit.py
+++ condor_multi_submit.py
@@ -5,33 +5,33 @@ def add_job(tag, options, loc):
 
 ##############################################################################
 
-output_dir       = 'dev_testing'
-job_tag          = 'fullrun_3'
+output_dir       = 'fullfun'
+job_tag          = 'first_test_with_tight'
 common_options   = [
 '--lumi=59830'
 ]
 jobdir_prefix    = job_tag + '_'
-full_output_path = '/cms/chiarito/atto_plotting/' + output_dir + '/' + job_tag
+full_output_path = '/cms/chiarito/condor/atto_plotting/' + output_dir + '/' + job_tag
 
 ## mc must have --numJobs=1
 add_job(tag='egamma18a', options='--data --filesPerJob=10 --input_cmslpc',
-        loc='/store/user/bchiari1/atto/datamc_full/egamma18a/2023-01-16-21-15-25/fd_bd')
-add_job(tag='egamma18b', options='--data --input_cmslpc --datasetname=egamma18b --filesPerJob=25',
-        loc='/store/user/bchiari1/atto/datamc_full/egamma18b/2023-01-16-21-15-28/fd_bd')
-add_job(tag='egamma18c', options='--data --input_cmslpc --datasetname=egamma18c --filesPerJob=25',
-        loc='/store/user/bchiari1/atto/datamc_full/egamma18c/2023-01-16-21-15-29/fd_bd')
-add_job(tag='egamma18d', options='--data --datasetname=egamma18d --filesPerJob=1000',
-        loc='/cms/chiarito/rootfiles/atto/egamma_18d')
+        loc='/store/user/bchiari1/atto/fullrun/w_tight2egamma18a/2023-02-01-19-26-01/fd_bd')
+add_job(tag='egamma18b', options='--data --input_cmslpc --filesPerJob=25',
+        loc='/store/user/bchiari1/atto/fullrun/w_tight2egamma18b/2023-02-01-19-26-04/fd_bd')
+add_job(tag='egamma18c', options='--data --input_cmslpc --filesPerJob=25',
+        loc='/store/user/bchiari1/atto/fullrun/w_tight2egamma18c/2023-02-01-19-26-06/fd_bd')
+add_job(tag='egamma18d', options='--data --filesPerJob=1000',
+        loc='/cms/chiarito/condor/atto/egamma_d_supplement_wtight2/2023-02-01-20-27-22/fd_bd')
 add_job(tag='gjets40to100', options='--mc --input_cmslpc --numJobs=1',
-        loc='/store/user/bchiari1/atto/datamc_full/gjets40to100/2023-01-16-21-15-30/fd_bd')
+        loc='/store/user/bchiari1/atto/fullrun/w_tight2gjets40to100/2023-02-01-19-26-07/fd_bd')
 add_job(tag='gjets100to200', options='--mc --input_cmslpc --numJobs=1',
-        loc='/store/user/bchiari1/atto/datamc_full/gjets100to200/2023-01-16-21-15-31/fd_bd')
+        loc='/store/user/bchiari1/atto/fullrun/w_tight2gjets100to200/2023-02-01-19-26-08/fd_bd')
 add_job(tag='gjets200to400', options='--mc --input_cmslpc --numJobs=1',
-        loc='/store/user/bchiari1/atto/datamc_full/gjets200to400/2023-01-16-21-15-31/fd_bd')
+        loc='/store/user/bchiari1/atto/fullrun/w_tight2gjets200to400/2023-02-01-19-26-09/fd_bd')
 add_job(tag='gjets400to600', options='--mc --input_cmslpc --numJobs=1',
-        loc='/store/user/bchiari1/atto/datamc_full/gjets400to600/2023-01-16-21-15-32/fd_bd')
+        loc='/store/user/bchiari1/atto/fullrun/w_tight2gjets400to600/2023-02-01-19-26-10/fd_bd')
 add_job(tag='gjets600toInf', options='--mc --input_cmslpc --numJobs=1',
-        loc='/store/user/bchiari1/atto/datamc_full/gjets600toInf/2023-01-16-21-15-32/fd_bd')
+        loc='/store/user/bchiari1/atto/fullrun/w_tight2gjets600toInf/2023-02-01-19-26-11/fd_bd')
 
 ##############################################################################
 
