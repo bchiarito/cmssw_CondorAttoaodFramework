882ddb3713eae88474145063557ece6c89bf056a
Brandon Chiarito <bchiarito@gmail.com>
2023-02-07 16:05:57 -0500

    mutlirunner atto, now based on local to hex

--- multi_submit_atto.py
+++ multi_submit_atto.py
@@ -5,34 +5,35 @@ def add_job(tag, options, loc):
 
 ##############################################################################
 
-job_tag          = 'fullrun_feb4'
+job_tag          = 'fullrun2_loctohex_wfilter_feb6'
 output_dir       = 'twoprong/sanity_plots/atto'
 common_options   = [
-'--filesPerJob=40',
+'--filesPerJob=20',
 '--scheddLimit=100',
+'--filter="one_hpid_photon"',
 '--recophiphoton=HPID'
 ]
 jobdir_prefix    = job_tag + '_'
 full_output_path = '/cms/chiarito/eos/' + output_dir + '/' + job_tag
 
-add_job(tag='egamma18a', options='--data --input_cmslpc --datasetname=egamma18a',
-        loc='/store/user/bchiari1/full_dataset_runs_egamma_18a_full/2022-08-23-18-32-47/fv1p4-10-44c4_bv1p1-1-331c')
-add_job(tag='egamma18b', options='--data --input_cmslpc --datasetname=egamma18b',
-        loc='/store/user/lpcrutgers/sthayil/pseudoaxions/nano/egamma2018b_09-22/2022-09-16-10-48-54/fv1p4-12-b296_bv1p1-1-331c')
-add_job(tag='egamma18c', options='--data --input_cmslpc --datasetname=egamma18c',
-        loc='/store/user/bchiari1/full_dataset_runs/egamma_18c_full/2022-12-05-20-00-01/fv1p4-16-9f1a_bv1p1-1-331c')
+add_job(tag='egamma18a', options='--data --datasetname=egamma18a',
+        loc='/cms/twoprong/chiarito/nano/egamma_18a/fv1p4-10-44c4_bv1p1-1-331c')
+add_job(tag='egamma18b', options='--data --datasetname=egamma18b',
+        loc='/cms/twoprong/chiarito/nano/egamma_18b/fv1p4-12-b296_bv1p1-1-331c')
+add_job(tag='egamma18c', options='--data --datasetname=egamma18c',
+        loc='/cms/twoprong/chiarito/nano/egamma_18c/fv1p4-16-9f1a_bv1p1-1-331c')
 add_job(tag='egamma18d', options='--data --datasetname=egamma18d',
-        loc='/cms/twoprong/jwf82/condor_miniaod_runs/egamma_datasets/egamma_2018D/2022-08-24-09-17-01')
-add_job(tag='gjets40to100', options='--mc --input_cmslpc --xs=20810 --datasetname=gjets40to100',
-        loc='/store/user/bchiari1/full_dataset_runs/gjets_40to100_UL18_10per/2022-09-06-10-29-01/fv1p4-12-b296_bv1p1-1-331c/')
-add_job(tag='gjets100to200', options='--mc --input_cmslpc --xs=9223 --datasetname=gjets100to200',
-        loc='/store/user/bchiari1/full_dataset_runs/gjets_100to200_UL18_10per/2022-09-10-14-52-10/fv1p4-12-b296_bv1p1-1-331c/')
-add_job(tag='gjets200to400', options='--mc --input_cmslpc --xs=2303 --datasetname=gjets200to400',
-        loc='/store/user/bchiari1/full_dataset_runs/gjets_200to400_UL18_10per/2022-11-22-20-09-46/fv1p4-16-9f1a_bv1p1-1-331c/')
-add_job(tag='gjets400to600', options='--mc --input_cmslpc --xs=274.5 --datasetname=gjets400to600',
-        loc='/store/user/bchiari1/full_dataset_runs/gjets_400to600_UL18_10per/2022-09-05-12-58-43/fv1p4-12-b296_bv1p1-1-331c/')
-add_job(tag='gjets600toInf', options='--mc --input_cmslpc --xs=93.52 --datasetname=gjets600toInf',
-        loc='/store/user/bchiari1/full_dataset_runs/gjets_600toInf_UL18_10per/2022-09-05-13-02-50/fv1p4-12-b296_bv1p1-1-331c/')
+        loc='/cms/twoprong/chiarito/nano/egamma_18d/')
+add_job(tag='gjets40to100', options='--mc --xs=20810 --datasetname=gjets40to100',
+        loc='/cms/twoprong/chiarito/nano/gjets_10per/gjets40to100/')
+add_job(tag='gjets100to200', options='--mc --xs=9223 --datasetname=gjets100to200',
+        loc='/cms/twoprong/chiarito/nano/gjets_10per/gjets100to200')
+add_job(tag='gjets200to400', options='--mc --xs=2303 --datasetname=gjets200to400',
+        loc='/cms/twoprong/chiarito/nano/gjets_10per/gjets200to400')
+add_job(tag='gjets400to600', options='--mc --xs=274.5 --datasetname=gjets400to600',
+        loc='/cms/twoprong/chiarito/nano/gjets_10per/gjets400to600')
+add_job(tag='gjets600toInf', options='--mc --xs=93.52 --datasetname=gjets600toInf',
+        loc='/cms/twoprong/chiarito/nano/gjets_10per/gjets600toInf')
 
 ##############################################################################
 
