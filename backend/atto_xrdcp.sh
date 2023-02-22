49f82b6af8395deefa3177ace6ba0fbb3bbb3e69
Brandon Chiarito <bchiarito@gmail.com>
2023-02-22 14:43:47 -0500

    simpler backend script naming

--- backend/atto_xrdcp.sh
+++ backend/atto_xrdcp.sh
@@ -4,7 +4,7 @@ if [[ -z "$1" ]] ; then
   exit 1
 fi
 rm backend_atto_cmssw/CMSSW_10_6_20.tgz
-tar -C backend_atto_cmssw --exclude-vcs --exclude="*.root" -zcf CMSSW_10_6_20.tgz CMSSW_10_6_20
+tar -C backend_atto_cmssw --exclude="*.root" -zcf CMSSW_10_6_20.tgz CMSSW_10_6_20
 xrdfs root://cmseos.fnal.gov/ rm /store/user/$1/tarballs/atto/CMSSW_10_6_20.tgz
 xrdcp CMSSW_10_6_20.tgz root://cmseos.fnal.gov//store/user/$1/tarballs/atto/CMSSW_10_6_20.tgz
 mv CMSSW_10_6_20.tgz backend_atto_cmssw
--- backend/plotting_xrdcp.sh
+++ backend/plotting_xrdcp.sh
@@ -4,7 +4,7 @@ if [[ -z "$1" ]] ; then
   exit 1
 fi
 rm backend_plotting_cmssw/CMSSW_10_6_20.tgz
-tar -C backend_plotting_cmssw --exclude-vcs --exclude="*.root" -zcf CMSSW_10_6_20.tgz CMSSW_10_6_20
+tar -C backend_plotting_cmssw --exclude="*.root" -zcf CMSSW_10_6_20.tgz CMSSW_10_6_20
 xrdfs root://cmseos.fnal.gov/ rm /store/user/$1/tarballs/plotting/CMSSW_10_6_20.tgz
 xrdcp CMSSW_10_6_20.tgz root://cmseos.fnal.gov//store/user/$1/tarballs/plotting/CMSSW_10_6_20.tgz
 mv CMSSW_10_6_20.tgz backend_plotting_cmssw
