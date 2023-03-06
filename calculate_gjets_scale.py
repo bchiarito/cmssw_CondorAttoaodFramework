d9440c4ca78442d55d816f79082d7c286744119a
Brandon Chiarito <bchiarito@gmail.com>
2023-03-06 14:18:28 -0500

    move to two step plotter, calcuate then plot

---
+++ calculate_gjets_scale.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import os
+import sys
+import imp
+import argparse
+from functools import reduce
+import ROOT
+import helper.plotting_util as util
+
+# command line options
+parser = argparse.ArgumentParser(description="Makes pdf from atto+nano job directories")
+parser.add_argument("prefix", help='prefix of job directories, include "Job_" part')
+parser.add_argument("--out", default='plots', help='prefix for the output pdf files')
+parser.add_argument("--scale", action='store_true', default=False, help='scale data and mc to unit integral')
+args = parser.parse_args()
+
+# constants
+plotting_jobs = 'plotting_jobs/'
+gjets_color = ROOT.kGreen
+dy_color = ROOT.kViolet
+qcd_color = ROOT.kOrange #41
+signal_color = ROOT.kRed
+ROOT.gStyle.SetOptStat(0)
+main = args.out+'.pdf'
+cutflow = args.out+'_cutflow.pdf'
+SIGNAL_NORM = 10000
+lumi = 59830 # pb^-1
+
+# define job directories
+# data
+datadirs = []
+datadirs.append(args.prefix + 'egamma18a/' + plotting_jobs)
+datadirs.append(args.prefix + 'egamma18b/' + plotting_jobs)
+datadirs.append(args.prefix + 'egamma18c/' + plotting_jobs)
+datadirs.append(args.prefix + 'egamma18d/' + plotting_jobs)
+# mc
+gjetsdirs = []
+gjetsdirs.append(args.prefix + 'gjets40to100/' + plotting_jobs)
+gjetsdirs.append(args.prefix + 'gjets100to200/' + plotting_jobs)
+gjetsdirs.append(args.prefix + 'gjets200to400/' + plotting_jobs)
+gjetsdirs.append(args.prefix + 'gjets400to600/' + plotting_jobs)
+gjetsdirs.append(args.prefix + 'gjets600toInf/' + plotting_jobs)
+dydirs = []
+dydirs.append(args.prefix + 'dy50/' + plotting_jobs)
+qcddirs = []
+qcddirs.append(args.prefix + 'qcd50to100/' + plotting_jobs)
+qcddirs.append(args.prefix + 'qcd100to200/' + plotting_jobs)
+qcddirs.append(args.prefix + 'qcd200to300/' + plotting_jobs)
+qcddirs.append(args.prefix + 'qcd300to500/' + plotting_jobs)
+qcddirs.append(args.prefix + 'qcd700to1000/' + plotting_jobs)
+qcddirs.append(args.prefix + 'qcd1000to1500/' + plotting_jobs)
+qcddirs.append(args.prefix + 'qcd1500to2000/' + plotting_jobs)
+qcddirs.append(args.prefix + 'qcd2000toInf/' + plotting_jobs)
+# signal
+signaldirs = []
+signaldirs.append('Job_signal_500_eta/plotting_jobs/')
+
+# process into TH1's
+data_histos = util.get_flat_histo_collection(datadirs)
+gjets_histos = util.get_flat_histo_collection(gjetsdirs)
+dy_histos = util.get_flat_histo_collection(dydirs)
+qcd_histos = util.get_flat_histo_collection(qcddirs)
+signal_histos = util.get_flat_histo_collection(signaldirs)
+col_gjets_histos = util.get_histo_collection(gjetsdirs)
+col_qcd_histos = util.get_histo_collection(qcddirs)
+
+# diagnostic event totals
+for data_hist, gjets_hist, dy_hist, qcd_hist, signal_hist in zip(data_histos, gjets_histos, dy_histos, qcd_histos, signal_histos):
+  data_integral = data_hist.Integral() + data_hist.GetBinContent(0) + data_hist.GetBinContent(data_hist.GetNbinsX()+1)
+  gjets_integral = gjets_hist.Integral() + gjets_hist.GetBinContent(0) + gjets_hist.GetBinContent(gjets_hist.GetNbinsX()+1)
+  qcd_integral = qcd_hist.Integral() + qcd_hist.GetBinContent(0) + qcd_hist.GetBinContent(qcd_hist.GetNbinsX()+1)
+  dy_integral = dy_hist.Integral() + dy_hist.GetBinContent(0) + dy_hist.GetBinContent(dy_hist.GetNbinsX()+1)
+  signal_integral = signal_hist.Integral() + signal_hist.GetBinContent(0) + signal_hist.GetBinContent(signal_hist.GetNbinsX()+1)
+  print('')
+  print("Data Events {:,.0f}".format(data_integral))
+  print("GJets Events {:,.0f}".format(gjets_integral))
+  print("QCD Events {:,.0f}".format(qcd_integral))
+  print("DY Events {:,.0f}".format(dy_integral))
+  print("MC Events {:,.0f}".format(gjets_integral + qcd_integral + dy_integral))
+  print("Data/MC {}".format(data_integral/(gjets_integral + qcd_integral + dy_integral)))
+  print('')
+  break
+
+gjets_scale_factor = (data_integral - qcd_integral) / gjets_integral
+print("GJets factor:", gjets_scale_factor)
+
+print("")
+# cutflow plots
+for data_hist in data_histos:
+  if (data_hist.GetName()).startswith('cutflow'):
+    print("data trigger eff", float(data_hist.GetBinContent(4))/float(data_hist.GetBinContent(3)))
+for hists in zip(*col_gjets_histos):
+  if (hists[0].GetName()).startswith('cutflow'):
+    for i, hist in enumerate(hists):
+      if not hist.GetBinContent(3) == 0: print("gjets trigger eff", float(hist.GetBinContent(4))/float(hist.GetBinContent(3)))
+for dy_hist in dy_histos:
+  if (dy_hist.GetName()).startswith('cutflow'):
+    if not dy_hist.GetBinContent(3) == 0: print("dy trigger eff", float(dy_hist.GetBinContent(4))/float(dy_hist.GetBinContent(3)))
+for hists in zip(*col_qcd_histos):
+  if (hists[0].GetName()).startswith('cutflow'):
+    for i, hist in enumerate(hists):
+      if not hist.GetBinContent(3) == 0: print("qcd trigger eff", float(hist.GetBinContent(4))/float(hist.GetBinContent(3)))
+for signal_hist in signal_histos:
+  if (signal_hist.GetName()).startswith('cutflow'):
+    print("signal trigger eff", float(signal_hist.GetBinContent(4))/float(signal_hist.GetBinContent(3)))
--- pdf_plotter.py
+++ pdf_plotter.py
@@ -12,6 +12,7 @@ parser = argparse.ArgumentParser(description="Makes pdf from atto+nano job direc
 parser.add_argument("prefix", help='prefix of job directories, include "Job_" part')
 parser.add_argument("--out", default='plots', help='prefix for the output pdf files')
 parser.add_argument("--scale", action='store_true', default=False, help='scale data and mc to unit integral')
+parser.add_argument("-g", "--gjets_scale", type=float, default=1.21967655618, help='scale factor for gjets')
 args = parser.parse_args()
 
 # constants
@@ -21,9 +22,12 @@ dy_color = ROOT.kViolet
 qcd_color = ROOT.kOrange #41
 signal_color = ROOT.kRed
 ROOT.gStyle.SetOptStat(0)
+ROOT.gStyle.SetLegendFillColor(ROOT.TColor.GetColorTransparent(ROOT.kRed, 0.01));
+ROOT.gStyle.SetLegendBorderSize(0)
 main = args.out+'.pdf'
 cutflow = args.out+'_cutflow.pdf'
 SIGNAL_NORM = 10000
+GJETS_FACTOR = args.gjets_scale #1.199
 lumi = 59830 # pb^-1
 
 # define job directories
@@ -50,9 +54,11 @@ qcddirs.append(args.prefix + 'qcd300to500/' + plotting_jobs)
 qcddirs.append(args.prefix + 'qcd700to1000/' + plotting_jobs)
 qcddirs.append(args.prefix + 'qcd1000to1500/' + plotting_jobs)
 qcddirs.append(args.prefix + 'qcd1500to2000/' + plotting_jobs)
+qcddirs.append(args.prefix + 'qcd2000toInf/' + plotting_jobs)
 # signal
 signaldirs = []
 signaldirs.append('Job_signal_500_eta/plotting_jobs/')
+signaldirs.append('Job_signal_1200_0p5/plotting_jobs/')
 
 # process into TH1's
 data_histos = util.get_flat_histo_collection(datadirs)
@@ -60,25 +66,9 @@ gjets_histos = util.get_flat_histo_collection(gjetsdirs)
 dy_histos = util.get_flat_histo_collection(dydirs)
 qcd_histos = util.get_flat_histo_collection(qcddirs)
 signal_histos = util.get_flat_histo_collection(signaldirs)
+col_signal_histos = util.get_histo_collection(signaldirs)
 col_gjets_histos = util.get_histo_collection(gjetsdirs)
 col_qcd_histos = util.get_histo_collection(qcddirs)
-
-# diagnostic event totals
-for data_hist, gjets_hist, dy_hist, qcd_hist, signal_hist in zip(data_histos, gjets_histos, dy_histos, qcd_histos, signal_histos):
-  data_integral = data_hist.Integral()
-  gjets_integral = gjets_hist.Integral()
-  dy_integral = dy_hist.Integral()
-  qcd_integral = qcd_hist.Integral()
-  signal_integral = signal_hist.Integral()
-  print('')
-  print("Data Events {:,.0f}".format(data_integral))
-  print("GJets Events {:,.0f}".format(gjets_integral))
-  print("QCD Events {:,.0f}".format(qcd_integral))
-  print("DY Events {:,.0f}".format(dy_integral))
-  print("MC Events {:,.0f}".format(gjets_integral + qcd_integral + dy_integral))
-  print("Data/MC {}".format(data_integral/(gjets_integral + qcd_integral + dy_integral)))
-  print('')
-  break
 
 # start pdf files
 c = ROOT.TCanvas()
@@ -120,7 +110,6 @@ for data_hist in data_histos:
     data_hist.SetLineColor(ROOT.kBlack)
     data_hist.Draw()
     c.Print(cutflow)
-    print("data trigger eff", float(data_hist.GetBinContent(4))/float(data_hist.GetBinContent(3)))
 for hists in zip(*col_gjets_histos):
   if (hists[0].GetName()).startswith('cutflow'):
     for i, hist in enumerate(hists):
@@ -128,14 +117,12 @@ for hists in zip(*col_gjets_histos):
       hist.SetFillColor(gjets_color+i)
       hist.Draw('hist')
       c.Print(cutflow)
-      if not hist.GetBinContent(3) == 0: print("gjets trigger eff", float(hist.GetBinContent(4))/float(hist.GetBinContent(3)))
 for dy_hist in dy_histos:
   if (dy_hist.GetName()).startswith('cutflow'):
     dy_hist.SetLineColor(dy_color)
     dy_hist.SetFillColor(dy_color)
     dy_hist.Draw()
     c.Print(cutflow)
-    if not dy_hist.GetBinContent(3) == 0: print("dy trigger eff", float(dy_hist.GetBinContent(4))/float(dy_hist.GetBinContent(3)))
 for hists in zip(*col_qcd_histos):
   if (hists[0].GetName()).startswith('cutflow'):
     for i, hist in enumerate(hists):
@@ -143,12 +130,10 @@ for hists in zip(*col_qcd_histos):
       hist.SetFillColor(qcd_color+i)
       hist.Draw('hist')
       c.Print(cutflow)
-      if not hist.GetBinContent(3) == 0: print("qcd trigger eff", float(hist.GetBinContent(4))/float(hist.GetBinContent(3)))
 for signal_hist in signal_histos:
   if (signal_hist.GetName()).startswith('cutflow'):
     signal_hist.SetLineColor(signal_color)
     signal_hist.Draw()
-    print("signal trigger eff", float(signal_hist.GetBinContent(4))/float(signal_hist.GetBinContent(3)))
     c.Print(cutflow)
 
 # sanity plots
@@ -162,6 +147,7 @@ for data_hist, gjets_hist, dy_hist, qcd_hist, signal_hist in zip(data_histos, gj
   data_underflow = data_hist.GetBinContent(0)
   data_overflow = data_hist.GetBinContent(data_hist.GetNbinsX()+1)
   # prepare mc
+  if not gjets_hist.Integral()==0: gjets_hist.Scale(GJETS_FACTOR)
   hist_mcs = [dy_hist, qcd_hist, gjets_hist] # determines order in stack
   stack = ROOT.THStack('hs', 'hs')
   scale = 0
@@ -194,15 +180,15 @@ for data_hist, gjets_hist, dy_hist, qcd_hist, signal_hist in zip(data_histos, gj
   qcd_hist.SetFillColor(qcd_color)
   signal_hist.SetLineColor(signal_color)
   # legend
-  leg = ROOT.TLegend(0.75, 0.75, 1.0, 1.0)
-  leg.AddEntry(data_hist, 'EGamma UL18 {:,.0f} /pb'.format(lumi), 'l')
-  leg.AddEntry(hist_mcs[2], 'GJets', 'f')
-  leg.AddEntry(hist_mcs[0], 'DY m50', 'f')
-  leg.AddEntry(hist_mcs[1], 'QCD', 'f')
-  leg.AddEntry(signal_hist, 'Phi=500 GeV, omega=eta', 'f')
-  if not data_underflow==0 or not data_overflow==0: leg.AddEntry('', "Data {:,.0f}|{:,.0f}".format(data_underflow, data_overflow), '')
-  if not mc_underflow==0 or not mc_overflow==0: leg.AddEntry('', "MC {:,.0f}|{:,.0f}".format(mc_underflow, mc_overflow), '')
-  if not signal_underflow==0 or not signal_overflow==0: leg.AddEntry('', "Signal {:,.0f}|{:,.0f}".format(signal_underflow, signal_overflow), '')
+  leg = ROOT.TLegend(0.7, 0.60, 0.9, 0.9)
+  leg.AddEntry(data_hist, 'Data ({:,.0f})'.format(data_hist.Integral()), 'l')
+  leg.AddEntry(hist_mcs[2], 'GJets ({:,.0f})'.format(gjets_hist.Integral()), 'f')
+  leg.AddEntry(hist_mcs[0], 'DY m50 ({:,.0f})'.format(dy_hist.Integral()), 'f')
+  leg.AddEntry(hist_mcs[1], 'QCD ({:,.0f})'.format(qcd_hist.Integral()), 'f')
+  leg.AddEntry(signal_hist, 'Signal 500|eta ({:,.0f})'.format(signal_hist.Integral()), 'f')
+  if not data_underflow==0 or not data_overflow==0: leg.AddEntry('', "Data uo {:,.0f},{:,.0f}".format(data_underflow, data_overflow), '')
+  if not mc_underflow==0 or not mc_overflow==0: leg.AddEntry('', "MC uo {:,.0f},{:,.0f}".format(mc_underflow, mc_overflow), '')
+  if not signal_underflow==0 or not signal_overflow==0: leg.AddEntry('', "Signal uo {:,.0f},{:,.0f}".format(signal_underflow, signal_overflow), '')
   # draw linear
   c.SetLogy(0)
   data_hist.Draw()
