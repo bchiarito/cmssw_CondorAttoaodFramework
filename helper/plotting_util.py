3374e617d2fe61fe05bce46bd9dc444ce7e78aaa
Brandon Chiarito <bchiarito@gmail.com>
2023-10-17 13:31:37 -0400

    os path join stuff

--- helper/plotting_util.py
+++ helper/plotting_util.py
@@ -79,7 +79,7 @@ def get_effs(dir_list):
   '''
   effs = []
   for dir in dir_list:
-    metadata = get_meta(os.path.join(os.path.dirname(os.path.dirname(dir)),''))
+    metadata = get_meta(os.path.join(os.path.dirname(os.path.dirname(os.path.join(dir,''))),''))
     total = float(metadata['evtProcessed']); passfilter = float(metadata['evtPassDatafilter'])
     effs.append(passfilter/total)
   return effs
