51b8df6f599282e76ba3213a0f423c75a3e846c7
Brandon Chiarito <bchiarito@gmail.com>
2024-02-21 20:17:16 -0500

    finish adding histo step to multirunner

--- multi.py
+++ multi.py
@@ -9,15 +9,19 @@ from datetime import datetime, timedelta, date
 import yaml
 
 parser = argparse.ArgumentParser(description='Executes multiple condor_[submit/status].py commands')
-parser.add_argument('mode', choices=['atto', 'histo', 'status'], help='')
-parser.add_argument('-i', '--input', nargs='+', help='any number of input YAML files or Job_MultiJob_XXX directories')
+parser.add_argument('mode', choices=['atto', 'histo', 'status'], help='operation mode')
+parser.add_argument('input', nargs='+', help='any number of input YAML files or Job_MultiJob_XXX directories')
 parser.add_argument('-t', '--test', default=False, action='store_true', help="just print commands, don't execute")
-submit_args = parser.add_argument_group("atto mode")
-submit_args.add_argument('--name', default=None, help="append to 'name' parameter from .yml file")
-submit_args.add_argument('--intag', default=None, help="use in place of IN_TAG in .yml file")
-submit_args.add_argument('--manual', default=False, action='store_true', help="ask before processing each section of the input")
-submit_args.add_argument('--fullmanual', default=False, action='store_true', help="manually confirm all submissions")
-submit_args.add_argument('-f', '--force', default=False, action='store_true', help="add -f option")
+parser.add_argument('-f', '--force', default=False, action='store_true', help=argparse.SUPPRESS)
+atto_args = parser.add_argument_group("atto mode")
+atto_args.add_argument('--name', default='RUNNAME', help="append to 'name' parameter from .yml file")
+#atto_args.add_argument('--intag', default=None, help="use in place of IN_TAG in .yml file")
+atto_args.add_argument('--manual', default=False, action='store_true', help="ask before processing each section of the input")
+atto_args.add_argument('--fullmanual', default=False, action='store_true', help="manually confirm all submissions")
+histo_args = parser.add_argument_group("histo mode")
+histo_args.add_argument('--datamc', default='data', choices=['data', 'mc', 'sigRes', 'sigNonRes'], help='')
+histo_args.add_argument('--year', default='UL17', choices=['UL18', 'UL17', 'UL16'], help='')
+histo_args.add_argument('--lumi', default=59830, help='')
 status_args = parser.add_argument_group("status mode")
 status_args.add_argument('--full', default=False, action='store_true', help="don't use --summary")
 status_args.add_argument('--hadd', default=False, action='store_true', help="hadd all outputs")
@@ -33,9 +37,6 @@ if 'hexcms' in hostname: site = 'hexcms'
 elif 'fnal.gov' in hostname: site = 'cmslpc'
 elif 'cern.ch' in hostname: site = 'lxplus'
 else: raise SystemExit('ERROR: Unrecognized site: not hexcms, cmslpc, or lxplus')
-
-# init
-if not args.name and args.intag: args.name = args.intag
 
 # main
 if args.mode == 'status':
@@ -113,9 +114,9 @@ if args.mode == 'histo':
       mode = "plotting"
       job_input = output_area
       job_output = "-" #config["dest"] + (args.name if args.name else "") + config["dests"][i]
-      datamc = "--mc"
-      lumi = "--lumi=59830"
-      year = "--year=UL18"
+      lumi = "--lumi=" + args.lumi
+      year = "--year=" + args.year
+      datamc = "--" + args.datamc
       options = " ".join([datamc, lumi, "--plotter=sanity", "--photon=CBL220", "--filesPerJob=4", year])
       if not args.fullmanual: options += " --auto"
       if args.force: options += " --force"
@@ -136,8 +137,7 @@ if args.mode == 'atto':
     for config in jobs:
       if not config: continue
       try:
-        if args.name: parent_dir = "_".join(["MultiJob", args.name, config["name"]])
-        else: parent_dir = "_".join(["MultiJob", config["name"]])
+        parent_dir = "_".join(["MultiJob", args.name, config["name"]])
         N_subjobs = len(config["inputs"])
         assert N_subjobs == len(config["dests"]), "ERROR: lists 'inputs' and 'dests' are not the same length in yaml file!"
         print("Job", config["name"], "has", N_subjobs, "subjob(s):\n")
@@ -155,12 +155,10 @@ if args.mode == 'atto':
             script = "./condor_submit.py"
             mode = config["mode"]
             job_input = config["inputs"][i]
-            if IN_TAG in job_input:
-              if args.intag: job_input = job_input.replace(IN_TAG, args.intag)
-              else: raise SystemExit("ERROR: yaml contains {} but no option --intag specified!".format(IN_TAG))
-            job_output = config["dest"] + (args.name if args.name else "") + config["dests"][i]
-            if args.name: job_output = "/".join([config["dest"], args.name, config["dests"][i]])
-            else: job_output = "/".join([config["dest"], config["dests"][i]])
+            #if IN_TAG in job_input:
+            #  if args.intag: job_input = job_input.replace(IN_TAG, args.intag)
+            #  else: raise SystemExit("ERROR: yaml contains {} but no option --intag specified!".format(IN_TAG))
+            job_output = "/".join([config["dest"], args.name, config["dests"][i]])
             job_output = os.path.normpath(job_output)
             options = " ".join((config["common_options"]))
             if "options" in config:
