67a3c16597ca5b30fb11b4f4aa7c371ff7ca4386
Brandon Chiarito <bchiarito@gmail.com>
2024-07-22 12:02:16 -0400

    wip to get working on cmslpc as well

--- backend/atto_get.sh
+++ backend/atto_get.sh
@@ -1,11 +1,11 @@
 #! /bin/sh
 
-#if [[ "hexcms" == "hexcms" ]]; then
+if [[ $1 == "hexcms" ]]; then
 
 export PATH="/cvmfs/oasis.opensciencegrid.org/mis/apptainer/1.2.5/bin:$PATH"
 cmssw-el7 "--bind /condor --bind /osg --bind /cms --bind /home --bind /users" -- backend/aux_atto_get.sh
 
-#elif [[ "hexcms" == "cmslpc" ]]; then
-#cmssw-el7 "--bind /uscms_data/d1/$USER/" -- backend/aux_atto_get.sh
+elif [[ $1 == "cmslpc" ]]; then
+cmssw-el7 "--bind /uscms_data/d1/$USER/" -- backend/aux_atto_get.sh
 
-#fi
+fi
--- backend/plotting_get.sh
+++ backend/plotting_get.sh
@@ -1,29 +1,11 @@
 #! /bin/sh
-rm -rf backend_plotting_cmssw
-mkdir backend_plotting_cmssw
-cd backend_plotting_cmssw
-cmsrel CMSSW_10_6_20
-cd CMSSW_10_6_20/src
-git clone https://github.com/cms-nanoAOD/nanoAOD-tools.git PhysicsTools/NanoAODTools
-cd PhysicsTools/NanoAODTools/python
-if [[ -z "$1" ]] ; then
-  echo ""
-  echo "Got no argument, using default (https://github.com/bchiarito/cmssw_backend_attoplotting.git) github repo for backend"
-  echo ""
-  git clone https://github.com/bchiarito/cmssw_backend_attoplotting.git fmk_plotting
-else
-  echo ""
-  echo "Using argument as github repo"
-  echo "git clone" $1 "test"
-  echo ""
-  git clone $1 fmk_plotting
-  exitcode=$?
-  if [[ exitcode -eq 0 ]] ; then
-      :
-  else
-      echo 'ERROR: git clone exited with non-zero exit code!'
-      exit 1
-  fi
-fi
-cd ../../..
-scram b
+
+#if [[ "hexcms" == "hexcms" ]]; then
+
+export PATH="/cvmfs/oasis.opensciencegrid.org/mis/apptainer/1.2.5/bin:$PATH"
+cmssw-el7 "--bind /condor --bind /osg --bind /cms --bind /home --bind /users" -- backend/aux_plotting_get.sh
+
+#elif [[ "hexcms" == "cmslpc" ]]; then
+#cmssw-el7 "--bind /uscms_data/d1/$USER/" -- backend/aux_atto_get.sh
+
+#fi
