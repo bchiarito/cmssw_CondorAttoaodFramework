8dc2a917d815a06a4a168268b2cff09aadab0d98
Brandon Chiarito <bchiarito@gmail.com>
2024-08-02 11:14:16 -0400

    working hadd_histo_dirs.py

--- hadd_histo_dirs.py
+++ hadd_histo_dirs.py
@@ -12,7 +12,7 @@ import helper.plotting_util as util
 # command line options
 parser = argparse.ArgumentParser(description="sums with hadd for atto/histo job output")
 parser.add_argument("multidirs", nargs='+', help='Job_ or MultiJob_ dir or eos area')
-parser.add_argument("--out", help='')
+parser.add_argument("-o", "--out", help='')
 parser.add_argument("--retain", action='store_true', default=False, help='')
 parser.add_argument("--rehadd", action='store_true', default=False, help='')
 parser.add_argument("--nomove", action='store_true', default=False, help='')
@@ -32,13 +32,38 @@ for multijob_dir in multijob_dirs:
   if args.rehadd: shutil.rmtree(hadd_dir)
   if not os.path.isdir(hadd_dir): os.mkdir(hadd_dir)
   summed_multijob = os.path.join(hadd_dir, "fullsum_{}.root".format(os.path.dirname(multijob_dir+'/')[13:]))
-  multijob_subdirs = os.listdir(multijob_dir)
-  summed_files = []
-  for subdir in multijob_subdirs:
-    if not os.path.isdir(os.path.join(multijob_dir, subdir)): continue
-    if subdir == hadd_dir_name: continue
-    job_dir = os.path.join(multijob_dir, subdir)
-    sys.path.append(os.path.join(multijob_dir, subdir))
+  if multijob_dir.startswith("MultiJob_"):
+      multijob_subdirs = os.listdir(multijob_dir)
+      summed_files = []
+      for subdir in multijob_subdirs:
+        if not os.path.isdir(os.path.join(multijob_dir, subdir)): continue
+        if subdir == hadd_dir_name: continue
+        job_dir = os.path.join(multijob_dir, subdir)
+        sys.path.append(os.path.join(multijob_dir, subdir))
+        import job_info as job
+        output_area = job.output
+        sys.path.pop()
+        sys.modules.pop("job_info")
+        output_list = os.listdir(output_area)
+        rootfiles = []
+        for item in output_list:
+          if not os.path.isfile(os.path.join(output_area, item)): continue
+          if not item.endswith(".root"): continue
+          rootfiles.append(os.path.join(output_area, item))
+        summed_file = os.path.join(hadd_dir, "sum_{}_{}.root".format(os.path.dirname(multijob_dir+'/')[13:], subdir))
+        summed_files.append(summed_file)
+        command = " ".join(["hadd -f", summed_file, " ".join(rootfiles)])
+        if os.path.isfile(summed_file): continue
+        os.system(command)
+      command = " ".join(["hadd -f", summed_multijob, " ".join(summed_files)])
+      if os.path.isfile(summed_multijob): continue
+      os.system(command)
+      final_sum_filename = args.out if args.out else summed_multijob
+      if not args.nomove: os.system("mv -f "+summed_multijob+" ./"+final_sum_filename)
+      if not args.retain: shutil.rmtree(hadd_dir)
+  elif multijob_dir.startswith("Job_"):
+    job_dir = multijob_dir 
+    sys.path.append(job_dir)
     import job_info as job
     output_area = job.output
     sys.path.pop()
@@ -49,14 +74,10 @@ for multijob_dir in multijob_dirs:
       if not os.path.isfile(os.path.join(output_area, item)): continue
       if not item.endswith(".root"): continue
       rootfiles.append(os.path.join(output_area, item))
-    summed_file = os.path.join(hadd_dir, "sum_{}_{}.root".format(os.path.dirname(multijob_dir+'/')[13:], subdir))
-    summed_files.append(summed_file)
+    summed_file = os.path.join(hadd_dir, "sum.root".format(job_dir))
     command = " ".join(["hadd -f", summed_file, " ".join(rootfiles)])
-    if os.path.isfile(summed_file): continue
-    os.system(command)
-  command = " ".join(["hadd -f", summed_multijob, " ".join(summed_files)])
-  if os.path.isfile(summed_multijob): continue
-  os.system(command)
-  final_sum_filename = args.out if args.out else summed_multijob
-  if not args.nomove: os.system("mv -f "+summed_multijob+" ./"+final_sum_filename)
-  if not args.retain: shutil.rmtree(hadd_dir)
+    if not os.path.isfile(summed_file):
+        os.system(command)
+    final_sum_filename = args.out if args.out else summed_multijob
+    if not args.nomove: os.system("mv -f "+summed_file+" ./"+final_sum_filename)
+    if not args.retain: shutil.rmtree(hadd_dir)
